d0134013167128446b4fc0ee5de3b89ef1f93e54
Manjusaka <80582093+ManjusakaY@users.noreply.github.com>
2023-03-16 12:02:10 +0800

    Update Manjusaka_Dpi.cpp

--- Source/Manjusaka_Dpi.cpp
+++ Source/Manjusaka_Dpi.cpp
@@ -11,17 +11,17 @@ using namespace std;
 const string CONFIG_DIR_PATH = "/data/media/0/Android/Manjusaka/自适应Dpi";
 const string CONFIG_FILE_PATH = CONFIG_DIR_PATH + "/Manjusaka_Dpi.conf";
 const string LOG_FILE_PATH = CONFIG_DIR_PATH + "/Manjusaka_Dpi.log";
-const string DEFAULT_Dpi_CMD = "wm density | grep Physical | cut -d ':' -f2 | xargs echo";
+const string DEFAULT_RESOLUTION_CMD = "wm density | grep Physical | cut -d ':' -f2 | xargs echo";
 const string CURRENT_APP_NAME_CMD = "am stack list|awk '/taskId/&&!/unknown/{print$2}'| awk -F: 'NR==1'|awk -F '/' '{print $1}'";
-const string CURRENT_Dpi_CMD_1 = "wm density | grep Physical | cut -d ':' -f2 | xargs echo";
-const string CURRENT_Dpi_CMD_2 = "wm density | grep Override | cut -d ':' -f2 | xargs echo";
+const string CURRENT_RESOLUTION_CMD_1 = "wm density | grep Physical | cut -d ':' -f2 | xargs echo";
+const string CURRENT_RESOLUTION_CMD_2 = "wm density | grep Override | cut -d ':' -f2 | xargs echo";
 ofstream log_f1(LOG_FILE_PATH.c_str());
 
 // 获取默认Dpi
-string get_default_Dpi()
+string get_default_resolution()
 {
-    auto start_time = chrono::high_Dpi_clock::now(); // 开始计时
-    FILE *fp = popen(DEFAULT_Dpi_CMD.c_str(), "r");
+    auto start_time = chrono::high_resolution_clock::now(); // 开始计时
+    FILE *fp = popen(DEFAULT_RESOLUTION_CMD.c_str(), "r");
     if (fp == NULL)
     {
         std::string date_str = "";
@@ -45,7 +45,7 @@ string get_default_Dpi()
     string default_res = string(buf);
     default_res.erase(default_res.find_last_not_of("\n") + 1);
 
-    auto end_time = chrono::high_Dpi_clock::now(); // 结束计时
+    auto end_time = chrono::high_resolution_clock::now(); // 结束计时
     auto time_diff = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
     return default_res;
 }
@@ -53,7 +53,7 @@ string get_default_Dpi()
 // 获取当前应用程序名称
 string get_current_app_name()
 {
-    auto start_time = chrono::high_Dpi_clock::now(); // 开始计时
+    auto start_time = chrono::high_resolution_clock::now(); // 开始计时
     auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
     FILE *fp = popen(CURRENT_APP_NAME_CMD.c_str(), "r");
     if (fp == NULL)
@@ -79,19 +79,19 @@ string get_current_app_name()
     string app_name = string(buf);
     app_name.erase(app_name.find_last_not_of("\n") + 1);
 
-    auto end_time = chrono::high_Dpi_clock::now(); // 结束计时
+    auto end_time = chrono::high_resolution_clock::now(); // 结束计时
     auto time_diff = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
     return app_name;
 }
 // 获取当前Dpi
-string get_current_Dpi()
+string get_current_resolution()
 {
-    auto start_time = chrono::high_Dpi_clock::now(); // 开始计时
+    auto start_time = chrono::high_resolution_clock::now(); // 开始计时
     auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
 
     string current_res;
     {
-        ifstream ifs(CURRENT_Dpi_CMD_1.c_str());
+        ifstream ifs(CURRENT_RESOLUTION_CMD_1.c_str());
         getline(ifs, current_res);
         ifs.close();
         current_res.erase(current_res.find_last_not_of("\n") + 1);
@@ -100,14 +100,14 @@ string get_current_Dpi()
     if (current_res.empty())
     {
         {
-            ifstream ifs(CURRENT_Dpi_CMD_2.c_str());
+            ifstream ifs(CURRENT_RESOLUTION_CMD_2.c_str());
             getline(ifs, current_res);
             ifs.close();
             current_res.erase(current_res.find_last_not_of("\n") + 1);
         }
     }
 
-    auto end_time = chrono::high_Dpi_clock::now(); // 结束计时
+    auto end_time = chrono::high_resolution_clock::now(); // 结束计时
     auto time_diff = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
     return current_res;
 }
@@ -125,7 +125,7 @@ int main()
     ofstream log_file(LOG_FILE_PATH.c_str());
     chmod(LOG_FILE_PATH.c_str(), S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 
-    string default_res = get_default_Dpi();
+    string default_res = get_default_resolution();
     string prev_res;
     bool is_restored_default_res = false;
     bool kunkun = false;
@@ -147,13 +147,13 @@ int main()
 
     while (true)
     {
-        auto loop_start_time = chrono::high_Dpi_clock::now(); // 记录每次循环开始的时间
+        auto loop_start_time = chrono::high_resolution_clock::now(); // 记录每次循环开始的时间
 
         time_t cur_time = time(NULL);
         tm *timeinfo = gmtime(&cur_time);
 
         string current_app_name = get_current_app_name();
-        string current_res = get_current_Dpi();
+        string current_res = get_current_resolution();
 
         // 读取配置文件，查找是否存在当前应用程序的包名
         bool found = false;
@@ -259,7 +259,7 @@ int main()
             }
         }
 
-        auto loop_end_time = chrono::high_Dpi_clock::now(); // 记录每次循环结束的时间
+        auto loop_end_time = chrono::high_resolution_clock::now(); // 记录每次循环结束的时间
         auto loop_time_diff = chrono::duration_cast<chrono::microseconds>(loop_end_time - loop_start_time);
      int sleep_time = std::max<long long>(1000000 - loop_time_diff.count(), 0);
         usleep(sleep_time); // 微秒级别的休眠
