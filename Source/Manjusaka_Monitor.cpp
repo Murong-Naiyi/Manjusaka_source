5b6c004e280586fc4d3c666b84b18f0f63c7b903
慕容奈依 <94782807+Murong-Naiyi@users.noreply.github.com>
2023-03-17 20:10:56 +0800

    Update Manjusaka_Monitor.cpp

--- Source/Manjusaka_Monitor.cpp
+++ Source/Manjusaka_Monitor.cpp
@@ -7,7 +7,7 @@
 #include <sys/stat.h>
 
 // 配置文件路径
-std::string Manjusaka_conf_path = "./_纯白恋歌.cblg";
+std::string Manjusaka_conf_path = "./_純白恋歌.cblg";
 void check_screen(std::string Set_Time, std::string Creen_script, std::string Screen_script, std::string Screen_switch) {
     auto check = [&]() {
         if (Screen_switch != "Y") return; // 当 Screen_switch=N 时，退出函数
